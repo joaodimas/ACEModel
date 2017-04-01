d1c4d80f3ccdf58111d8dbe509e952bfa9a16c9f
joaodimas <joaohenriqueavila@gmail.com>
2017-04-01 17:26:02 +0200

    Changing parameters

--- Parameters.py
+++ Parameters.py
@@ -10,11 +10,11 @@ class Parameters:
     
 
     # MODEL PARAMETERS
-    NumberOfSimulations = 500
+    NumberOfSimulations = 5
 
     # Basic
     NumberOfTasks = 96
-    NumberOfPotentialEntrants = 1
+    NumberOfPotentialEntrants = 40
     StartupWealth = 0
     ThresholdNetWealthForSurvival = 0 
     DemandIntercept = 300
@@ -25,7 +25,7 @@ class Parameters:
     InitialAttractionForNoResearch = 10
     InitialAttractionForInnovation = 10
     InitialAttractionForImitation = 10
-    TimeHorizon = 5000
+    TimeHorizon = 30
     MeanMarketSize = 4
     RateOfChangeInTechEnv = 0.1
     MaxMagnituteOfChangeInTechEnv = 8
