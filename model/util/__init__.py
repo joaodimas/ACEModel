e676d8d655f02a0dfcadf40f43281b48889c07ce
joaodimas <joaohenriqueavila@gmail.com>
2017-04-13 13:16:40 +0200

    Removed contents of __init__.py

--- model/util/__init__.py
+++
@@ -1,4 +0,0 @@
-# from model.util.combinatorics import Combinatorics
-# from model.util.export_to_csv import ExportToCSV
-# from model.util.logger import Logger
-# from model.util.random import Random
