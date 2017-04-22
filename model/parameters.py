2e86a727d165b9dbdaf708d7abf65234b0983991
joaodimas <joaohenriqueavila@gmail.com>
2017-04-22 18:57:55 +0200

    Parameters

--- model/parameters.py
+++ model/parameters.py
@@ -8,6 +8,33 @@ class Parameters:
     PeriodRangeToSavePanelData = [2100, 2140]
 
 
+    # ---------------- TESTS -----------------------
+    # 
+    NumberOfTasks = 96
+    NumberOfPotentialEntrants = 40
+    StartupWealth = 0
+    ThresholdNetWealthForSurvival = 0 
+    DemandIntercept = 300
+    FixedProductionCost = 200
+    FixedCostOfInnovation = 100
+    FixedCostOfImitation = 50
+    InitialAttractionForResearch = 10
+    InitialAttractionForNoResearch = 10
+    InitialAttractionForInnovation = 10
+    InitialAttractionForImitation = 10
+    TimeHorizon = 5000
+    MeanMarketSize = 128
+    RateOfChangeInTechEnv = 0.1
+    PeriodStartOfTechnologicalShocks = 0
+    MaxMagnituteOfChangeInTechEnv = 8
+    TypeOfCycle = CycleType.STOCHASTIC  # <----------- STOCHASTIC BUSINESS CYCLES
+    PeriodsOfConstantDemand = 2000
+    MinMarketSize = 0.1
+    RateOfPersistenceInDemand = 0.95
+    # 
+    # ------------------------------------------------------------------
+
+
     # ---------------- CHAPTER 4 of Chang's book -----------------------
     # 
     # NumberOfTasks = 96
@@ -58,27 +85,27 @@ class Parameters:
 
     # ---------------- CHAPTER 8 of Chang's book -----------------------
     # 
-    NumberOfTasks = 96
-    NumberOfPotentialEntrants = 40
-    StartupWealth = 0
-    ThresholdNetWealthForSurvival = 0 
-    DemandIntercept = 300
-    FixedProductionCost = 200
-    FixedCostOfInnovation = 100
-    FixedCostOfImitation = 50
-    InitialAttractionForResearch = 10
-    InitialAttractionForNoResearch = 10
-    InitialAttractionForInnovation = 10
-    InitialAttractionForImitation = 10
-    TimeHorizon = 5000
-    MeanMarketSize = 128
-    RateOfChangeInTechEnv = 0.1
-    PeriodStartOfTechnologicalShocks = 0
-    MaxMagnituteOfChangeInTechEnv = 8
-    TypeOfCycle = CycleType.STOCHASTIC  # <----------- STOCHASTIC BUSINESS CYCLES
-    PeriodsOfConstantDemand = 2000
-    MinMarketSize = 0.1
-    RateOfPersistenceInDemand = 0.95
+    # NumberOfTasks = 96
+    # NumberOfPotentialEntrants = 40
+    # StartupWealth = 0
+    # ThresholdNetWealthForSurvival = 0 
+    # DemandIntercept = 300
+    # FixedProductionCost = 200
+    # FixedCostOfInnovation = 100
+    # FixedCostOfImitation = 50
+    # InitialAttractionForResearch = 10
+    # InitialAttractionForNoResearch = 10
+    # InitialAttractionForInnovation = 10
+    # InitialAttractionForImitation = 10
+    # TimeHorizon = 5000
+    # MeanMarketSize = 4
+    # RateOfChangeInTechEnv = 0.1
+    # PeriodStartOfTechnologicalShocks = 0
+    # MaxMagnituteOfChangeInTechEnv = 8
+    # TypeOfCycle = CycleType.STOCHASTIC  # <----------- STOCHASTIC BUSINESS CYCLES
+    # PeriodsOfConstantDemand = 2000
+    # MinMarketSize = 0.1
+    # RateOfPersistenceInDemand = 0.95
     # 
     # ------------------------------------------------------------------
 
