56f84b3409a1d5d136a4a21f3dfc2f615e480d69
joaodimas <joaohenriqueavila@gmail.com>
2017-05-14 16:25:31 +0200

    Scripts

--- UpdateRunCommitShutdown.sh
+++ UpdateRunCommitShutdown.sh
@@ -6,6 +6,7 @@ git pull
 mkdir ./data/New_results
 rm ./data/*.log
 mv ./data/ACEModel.2017*MEAN* ./data/New_results/
+mv ./data/ACEModel.2017*params* ./data/New_results/
 rm ./data/ACEModel*
 git add .
 git commit -m "Results"
